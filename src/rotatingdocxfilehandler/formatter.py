e6e1b38c724e48cd8e2641ef505a5d289929eb9f
Torben Carstens <git@carstens.tech>
2022-01-08 18:48:04 +0100

    Add DocxFormatter
    Currently uses the default formatting impl

---
+++ src/rotatingdocxfilehandler/formatter.py
@@ -0,0 +1,23 @@
+from logging import Formatter
+
+
+class DocxFormatter(Formatter):
+    def format(self, record):
+        record.message = record.getMessage()
+        if self.usesTime():
+            record.asctime = self.formatTime(record, self.datefmt)
+        s = self.formatMessage(record)
+        if record.exc_info:
+            # Cache the traceback text to avoid converting it multiple times
+            # (it's constant anyway)
+            if not record.exc_text:
+                record.exc_text = self.formatException(record.exc_info)
+        if record.exc_text:
+            if s[-1:] != "\n":
+                s = s + "\n"
+            s = s + record.exc_text
+        if record.stack_info:
+            if s[-1:] != "\n":
+                s = s + "\n"
+            s = s + self.formatStack(record.stack_info)
+        return s
